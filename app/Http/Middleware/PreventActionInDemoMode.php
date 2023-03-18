d4058d691cf0bd3cfadf6f283f2bc98e9bc959fd
Neba Funwi-gabga <fgneba@gmail.com>
2023-03-18 12:38:12 -0400

    Added linked store

--- app/Http/Middleware/PreventActionInDemoMode.php
+++ app/Http/Middleware/PreventActionInDemoMode.php
@@ -16,26 +16,26 @@ class PreventActionInDemoMode
     public function handle(Request $request, Closure $next): Response
     {
 
-        if ((Auth::check() && Auth::user()->role == Roles::SUPER_ADMIN) || !app_demo()) {
-            return $next($request);
-        }
-
-        $protectedRoutes = Demo::DEMO_PROTECTED_ROUTES;
-
-        $currentRouteName = $request->route()->getName();
-        $currentRouteMethod = $request->method();
-
-        $isProtected = false;
-
-        foreach ($protectedRoutes as $route) {
-            if ($route['name'] == $currentRouteName && $route['method'] == $currentRouteMethod) {
-                $isProtected = true;
-                break;
-            }
-        }
-        if ($isProtected) {
-            return \Redirect::back()->error("Action is not allowed in Demo mode");
-        }
+//        if ((Auth::check() && Auth::user()->role == Roles::SUPER_ADMIN) || !app_demo()) {
+//            return $next($request);
+//        }
+//
+//        $protectedRoutes = Demo::DEMO_PROTECTED_ROUTES;
+//
+//        $currentRouteName = $request->route()->getName();
+//        $currentRouteMethod = $request->method();
+//
+//        $isProtected = false;
+//
+//        foreach ($protectedRoutes as $route) {
+//            if ($route['name'] == $currentRouteName && $route['method'] == $currentRouteMethod) {
+//                $isProtected = true;
+//                break;
+//            }
+//        }
+//        if ($isProtected) {
+//            return \Redirect::back()->error("Action is not allowed in Demo mode");
+//        }
 
 
         return $next($request);
